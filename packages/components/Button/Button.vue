77a563938961f0beb6e7f58bbafa9f4d4f2e9f32
Wanmov <1216120392@qq.com>
2024-06-17 18:17:23 +0800

    feat:Button组件属性补全，storybook测试

--- packages/components/Button/Button.vue
+++ packages/components/Button/Button.vue
@@ -14,13 +14,17 @@
       'is-disabled': disabled,
       'is-loading': loading,
     }"
+    @click="
+      (e: MouseEvent) => (useThrottle ? handleClickThrottle(e) : handleClick(e))
+    "
     ><slot></slot
   ></component>
 </template>
 
 <script lang="ts" setup>
 import { ref } from "vue";
-import type { ButtonProps } from "./types";
+import type { ButtonProps, ButtonEmits, ButtonInstance } from "./types";
+import { throttle } from "lodash-es";
 
 defineOptions({
   name: "WanButton",
@@ -28,11 +32,18 @@ defineOptions({
 const props = withDefaults(defineProps<ButtonProps>(), {
   tag: "button",
   nativeType: "button",
+  useThrottle: true,
+  throttleDuration: 500,
 });
+
+const emits = defineEmits<ButtonEmits>();
 
 const slots = defineSlots();
 
 const _ref = ref<HTMLButtonElement>();
+
+const handleClick = (e: MouseEvent) => emits("click", e);
+const handleClickThrottle = throttle(handleClick, props.throttleDuration);
 </script>
 <style scope>
 @import "./style.css";
---
+++ packages/play/src/stories/Button.stories.ts
@@ -0,0 +1,77 @@
+import type { Meta, StoryObj, ArgTypes } from "@storybook/vue3";
+import { fn } from "@storybook/test";
+
+import { WanButton } from "wan-element";
+
+type Story = StoryObj<typeof WanButton> & { argTypes: ArgTypes };
+
+const meta: Meta<typeof WanButton> = {
+  title: "Example/Button",
+  component: WanButton,
+  tags: ["autodocs"],
+  argTypes: {
+    type: {
+      control: { type: "select" },
+      options: ["primary", "success", "warning", "danger", "info", ""],
+    },
+    size: {
+      control: { type: "select" },
+      options: ["large", "default", "small", ""],
+    },
+    disabled: {
+      control: "boolean",
+    },
+    loading: {
+      control: "boolean",
+    },
+    useThrottle: {
+      control: "boolean",
+    },
+    throttleDuration: {
+      control: "number",
+    },
+    autofocus: {
+      control: "boolean",
+    },
+    tag: {
+      control: { type: "select" },
+      options: ["button", "a", "div"],
+    },
+    nativeType: {
+      control: { type: "select" },
+      options: ["button", "submit", "reset", ""],
+    },
+    icon: {
+      control: { type: "text" },
+    },
+    loadingIcon: {
+      control: { type: "text" },
+    },
+  },
+  args: { onClick: fn() },
+};
+
+const container = (val: string) => `<div style="margin:5px">${val}</div>`;
+
+export const Default: Story & { args: { content: string } } = {
+  argTypes: {
+    content: {
+      control: { type: "text" },
+    },
+  },
+  args: {
+    type: "primary",
+    content: "Button",
+  },
+  render: (args) => ({
+    components: { WanButton },
+    setup() {
+      return { args };
+    },
+    template: container(
+      `<wan-button v-bind="args">{{args.content}}</wan-button>`
+    ),
+  }),
+};
+
+export default meta;
