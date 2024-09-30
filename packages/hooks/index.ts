654b5be2d9955bff680954e96e0ec05e7561804e
Wanmov <1216120392@qq.com>
2024-09-30 16:30:04 +0800

    feat:禁用样式hook

--- packages/hooks/index.ts
+++ packages/hooks/index.ts
@@ -1,5 +1,6 @@
 import useEventListener from "./useEventListener";
 import useClickOutside from "./useClickOutside";
 import useId from "./useId";
+import useDisabledStyle from "./useDisabledStyle";
 
-export { useClickOutside, useEventListener, useId };
+export { useClickOutside, useEventListener, useId, useDisabledStyle };
