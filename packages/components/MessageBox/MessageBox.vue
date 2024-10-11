64848c542a2acc61e7c2be75c625f87f6f70ae3c
Wanmov <1216120392@qq.com>
2024-10-11 18:25:41 +0800

    feat:Overlay

---
+++ packages/components/MessageBox/MessageBox.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts" setup>
+defineOptions({
+  name: "WanMessageBox",
+  inheritAttrs: false,
+});
+</script>
+<style scope></style>
---
+++ packages/components/Overlay/Overlay.vue
@@ -0,0 +1,53 @@
+<template>
+  <div
+    v-if="mask"
+    class="wan-overlay"
+    :class="overlayClass"
+    :style="{ zIndex: zIndex }"
+    @click="onMaskClick"
+  >
+    <slot></slot>
+  </div>
+  <div
+    v-else
+    :class="overlayClass"
+    :style="{
+      zIndex: zIndex,
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+    }"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { OverlayProps, OverlayEmits } from "./types";
+
+defineOptions({ name: "ErOverlay" });
+withDefaults(defineProps<OverlayProps>(), {
+  mask: true,
+});
+const emits = defineEmits<OverlayEmits>();
+
+function onMaskClick(e: MouseEvent) {
+  emits("click", e);
+}
+</script>
+
+<style scoped>
+.wan-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  overflow: auto;
+  z-index: 2000;
+}
+</style>
