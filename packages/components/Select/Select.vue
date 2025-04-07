4fc0574fa89dac6aa7b270c2fb12ee032a36edee
Wanmov <1216120392@qq.com>
2025-04-07 19:13:58 +0800

    feat:Select(filter、keyEvent)

--- packages/components/Select/Select.vue
+++ packages/components/Select/Select.vue
@@ -23,10 +23,12 @@
             v-model="selectStates.inputValue"
             :id="inputId"
             :disabled="isDisabled"
-            :placeholder="placeholder"
+            :placeholder="filterable ? filterPlaceholder : placeholder"
             :readonly="!filterable || !isDropdownVisible"
             @focus="handleFocus"
             @blur="handleBlur"
+            @input="handleFilterDebounce"
+            @keydown="handleKeydown"
           >
             <template #suffix>
               <wan-icon
@@ -47,16 +49,27 @@
         </div>
       </template>
       <template #content>
+        <div class="wan-select__loading" v-if="selectStates.loading">
+          <wan-icon icon="spinner" spin />
+        </div>
+        <div class="wan-select__nodata" v-else-if="filterable && !hasData">
+          No data
+        </div>
         <ul class="wan-select__menu">
           <template v-if="!hasChildren">
             <wan-option
-              v-for="item in options"
+              v-for="item in filtedOptions"
               :key="item.value"
               v-bind="item"
             />
           </template>
           <template v-else>
-            <slot></slot>
+            <template
+              v-for="[vNode, _props] in filtedChilds"
+              :key="_props.value"
+            >
+              <render-vnode :vNode="vNode" />
+            </template>
           </template>
         </ul>
       </template>
@@ -84,10 +97,28 @@ import {
   watch,
   nextTick,
   type VNode,
+  h,
 } from "vue";
 import { POPPER_OPTIONS, SELECT_CTX_KEY } from "./constants";
 import { useClickOutside, useFocusController, useId } from "@wan-element/hooks";
-import { each, eq, filter, find, get, size, noop, isFunction } from "lodash-es";
+import {
+  each,
+  eq,
+  filter,
+  find,
+  get,
+  size,
+  noop,
+  isFunction,
+  map,
+  assign,
+  isNil,
+  isBoolean,
+  includes,
+  debounce,
+} from "lodash-es";
+import { debugWarn, RenderVnode } from "@wan-element/utils";
+import useKeyMap from "./useKeyMap";
 
 import WanOption from "./Option.vue";
 import WanTooltip from "../Tooltip/Tooltip.vue";
@@ -105,6 +136,8 @@ const slots = defineSlots();
 const selectRef = ref<HTMLElement>();
 const tooltipRef = ref<TooltipInstance>();
 const inputRef = ref<InputInstance>();
+const filtedChilds = ref<Map<VNode, SelectOptionProps>>(new Map());
+const filtedOptions = ref(props.options ?? []);
 
 const isDropdownVisible = ref(false);
 
@@ -127,17 +160,53 @@ const showClear = computed(
   () =>
     props.clearable && selectStates.mouseHover && selectStates.inputValue !== ""
 );
+
 const highlightedLine = computed(() => {
   let result: SelectOptionProps | void;
   if (hasChildren.value) {
-    const node = children.value[selectStates.highlightedIndex];
-    result = node.props.value;
+    const node = [...filtedChilds.value][selectStates.highlightedIndex]?.[0];
+    result = filtedChilds.value.get(node);
   } else {
-    result = props.options[selectStates.highlightedIndex];
+    result = filtedOptions.value[selectStates.highlightedIndex];
   }
 
   return result;
 });
+
+const childrenOptions = computed(() => {
+  if (!hasChildren.value) return [];
+
+  return map(children.value, (item) => ({
+    vNode: h(item),
+    props: assign(item.props, {
+      disabled:
+        item.props?.disabled === true ||
+        (!isNil(item.props?.disabled) && !isBoolean(item.props?.disabled)),
+    }),
+  }));
+});
+
+const hasData = computed(
+  () =>
+    (hasChildren.value && filtedChilds.value.size > 0) ||
+    (!hasChildren.value && size(filtedOptions.value) > 0)
+);
+
+const lastIndex = computed(() =>
+  hasChildren.value
+    ? filtedChilds.value.size - 1
+    : size(filtedOptions.value) - 1
+);
+
+const filterPlaceholder = computed(() =>
+  props.filterable && selectStates.selectedOption && isDropdownVisible.value
+    ? selectStates.selectedOption.label
+    : props.placeholder
+);
+
+const timeout = computed(() => (props.remote ? 300 : 100));
+
+const handleFilterDebounce = debounce(handleFilter, timeout.value);
 
 const inputId = useId().value;
 const {
@@ -146,6 +215,16 @@ const {
   handleBlur,
   handleFocus,
 } = useFocusController(inputRef);
+
+const keyMap = useKeyMap({
+  isDropdownVisible,
+  controlVisible,
+  selectStates,
+  highlightedLine,
+  handleSelect,
+  hasData,
+  lastIndex,
+});
 
 useClickOutside(selectRef, (e) => handleClickOutside(e));
 
@@ -166,10 +245,22 @@ function handleClickOutside(e?: Event) {
 function controlVisible(visible: boolean) {
   if (!tooltipRef.value) return;
   get(tooltipRef, ["value", visible ? "show" : "hide"])?.();
+  props.filterable && controlInputVal(visible);
   isDropdownVisible.value = visible;
   emits("visible-change", visible);
 
   selectStates.highlightedIndex = -1;
+}
+
+// DropMenu 切换 InputValue 变化
+function controlInputVal(visible: boolean) {
+  if (!props.filterable) return;
+  if (visible) {
+    if (selectStates.selectedOption) selectStates.inputValue = "";
+    handleFilterDebounce();
+    return;
+  }
+  selectStates.inputValue = selectStates.selectedOption?.label || "";
 }
 
 function toggleVisible() {
@@ -200,6 +291,95 @@ function handleSelect(opt: SelectOptionProps) {
   inputRef.value?.focus();
 }
 
+function handleFilter() {
+  const searcKey = selectStates.inputValue;
+  selectStates.highlightedIndex = -1;
+
+  if (hasChildren.value) {
+    genFilterChilds(searcKey);
+    return;
+  }
+  genFilterOptions(searcKey);
+}
+
+function handleKeydown(e: KeyboardEvent) {
+  keyMap.has(e.key) && keyMap.get(e.key)?.(e);
+}
+
+function setFiltedChilds(opts: typeof childrenOptions.value) {
+  filtedChilds.value.clear();
+  each(opts, (item) => {
+    filtedChilds.value.set(item.vNode, item.props as SelectOptionProps);
+  });
+}
+
+async function genFilterChilds(search: string) {
+  if (!props.filterable) return;
+
+  if (props.remote && props.remoteMethod && isFunction(props.remoteMethod)) {
+    // remote search
+    await callRemoteMethod(props.remoteMethod, search);
+    setFiltedChilds(childrenOptions.value);
+    return;
+  }
+
+  if (props.filterMethod && isFunction(props.filterMethod)) {
+    // filter search
+    const opts = map(props.filterMethod(search), "value");
+    setFiltedChilds(
+      filter(childrenOptions.value, (item) =>
+        includes(opts, get(item, ["props", "value"]))
+      )
+    );
+    return;
+  }
+
+  // default
+  setFiltedChilds(
+    filter(childrenOptions.value, (item) =>
+      includes(get(item, ["props", "label"]), search)
+    )
+  );
+}
+
+async function genFilterOptions(search: string) {
+  if (!props.filterable) return;
+
+  if (props.remote && props.remoteMethod && isFunction(props.remoteMethod)) {
+    // remote search
+    filtedOptions.value = await callRemoteMethod(props.remoteMethod, search);
+    return;
+  }
+
+  if (props.filterMethod && isFunction(props.filterMethod)) {
+    // filter search
+    filtedOptions.value = props.filterMethod(search);
+    return;
+  }
+
+  // default
+  filtedOptions.value = filter(props.options, (opt) =>
+    includes(opt.label, search)
+  );
+}
+
+async function callRemoteMethod(method: Function, search: string) {
+  if (!method || !isFunction(method)) return;
+
+  selectStates.loading = true;
+  let result;
+  try {
+    result = await method(search);
+  } catch (error) {
+    debugWarn(error as Error);
+    debugWarn("WanSelect", "callRemoteMethod error");
+    result = [];
+    return Promise.reject(error);
+  }
+
+  return result;
+}
+
 function renderLabel(opt: SelectOptionProps): VNode | string {
   if (isFunction(props.renderLabel)) {
     return props.renderLabel(opt);
@@ -213,6 +393,19 @@ function setSelected() {
   selectStates.inputValue = opt.label;
   selectStates.selectedOption = opt;
 }
+
+watch(
+  () => props.options,
+  (newVal) => {
+    filtedOptions.value = newVal ?? [];
+  }
+);
+
+watch(
+  () => childrenOptions.value,
+  (newVal) => setFiltedChilds(newVal),
+  { immediate: true }
+);
 
 watch(
   () => props.modelValue,
--- packages/play/src/App.vue
+++ packages/play/src/App.vue
@@ -4,11 +4,17 @@ import { ref } from "vue";
 const options = ref([
   { value: "beijing", label: "Beijing" },
   { value: "shanghai", label: "Shanghai" },
+  { value: "shenzhen", label: "Shenzhen", disabled: true },
+  { value: "hangzhou", label: "Hangzhou" },
 ]);
 const value = ref("");
 </script>
 
 <template>
-  <wan-select v-model="value" :options="options"></wan-select>
+  <wan-select
+    v-model="value"
+    :options="options"
+    filterable
+    clearable
+  ></wan-select>
 </template>
-
